cc9a7f05a17e5c62e64bdfcc07866d7d71196473
Björn Gylling <bjorn.gylling@gmail.com>
2010-03-19 15:19:56 +0100

    begun writing rules for rdparser and fixed some stuff in the grammar

--- lib/rdparse/rdparse.rb
+++ lib/rdparse/rdparse.rb
@@ -225,87 +225,94 @@ end
 
 ##############################################################################
 #
-# This part defines the dice language
+# Dunder language parser
 #
 ##############################################################################
 
-class DiceRoller
-        
-  def DiceRoller.roll(times, sides)
-    (1..times).inject(0) {|sum, _| sum + rand(sides) + 1 }
-  end
+class Dunder
   
   def initialize
-    @diceParser = Parser.new("dice roller") do
+    @dunder_parser = Parser.new("dunder") do
       token(/\s+/)
       token(/\d+/) {|m| m.to_i }
       token(/./) {|m| m }
       
-      start :expr do 
-        match(:expr, '+', :term) {|a, _, b| a + b }
-        match(:expr, '-', :term) {|a, _, b| a - b }
-        match(:term)
+      start :statement_list do 
+        match(:statement, :statement_terminator, :statement_list)
+        match(:statement, :statement_terminator)
       end
       
-      rule :term do 
-        match(:term, '*', :dice) {|a, _, b| a * b }
-        match(:term, '/', :dice) {|a, _, b| a / b }
-        match(:dice)
+      rule :statement_terminator do
+      	match(';')
+      	match("\n")
+    	end
+      
+      rule :statement do 
+      	match(:assignment_expression)
+      	match(:function_call)
+      	match(:if_statement)
+      	match(:while_statement)
+      	match(:function_def)
       end
 
-      rule :dice do
-        match(:atom, 'd', :sides) {|a, _, b| DiceRoller.roll(a, b) }
-        match('d', :sides) {|_, b| DiceRoller.roll(1, b) }
-        match(:atom)
+      rule :assignment_expression do
+        match(:identifier, '=', :expression)
       end
       
-      rule :sides do
-        match('%') { 100 }
-        match(:atom)
+      rule :function_call do
+        match(:identifier, :arguments)
       end
       
-      rule :atom do
-        # Match the result of evaluating an integer expression, which
-        # should be an Integer
-        match(Integer)
-        match('(', :expr, ')') {|_, a, _| a }
-      end
-    end
-  end
-  
-  def done(str)
-    ["quit","exit","bye",""].include?(str.chomp)
-  end
-  
-  def roll
-    print "[diceroller] "
-    str = gets
-    if done(str) then
-      puts "Bye."
-    else
-      puts "=> #{@diceParser.parse str}"
-      roll
+      rule :arguments do
+      	match('(', :expression_list, ')')
+    	end
+      	
+			rule :expression_list do
+				match(:expression_list, ',', :expression)
+				match(:expression)
+			end
+			
+			rule :expression do
+				match('true')
+				match('false')
+				match(:number)
+				match(:string)
+				match(:indetifier)
+				match(:function_call)
+				match(:expression, :binary_operator, :expression)
+			end
+			
+			rule :number do
+				match(:integer)
+				match(:float)
+			end
+			
+			rule :integer do
+				match(:non_zero_digit, :digits)
+				match('0')
+			end
+			
+			rule :non_zero_digit do
+				match("1-9")
+			end
+			
+			rule :digits do
+				match(:digits, :digit)
+				match(:digit)
+			end
+			
+			rule :digit do
+				match("0-9")
+			end
+			
     end
   end
 
   def log(state = true)
     if state
-      @diceParser.logger.level = Logger::DEBUG
+      @dunder_parser.logger.level = Logger::DEBUG
     else
-      @diceParser.logger.level = Logger::WARN
+      @dunder_parser.logger.level = Logger::WARN
     end
   end
 end
-
-# Examples of use
-
-# irb(main):1696:0> DiceRoller.new.roll
-# [diceroller] 1+3
-# => 4
-# [diceroller] 1+d4
-# => 2
-# [diceroller] 1+d4
-# => 3
-# [diceroller] (2+8*d20)*3d6
-# => 306
-
