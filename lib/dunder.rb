81707c087ab873aa42d4cc4815571caac36170d6
Björn Gylling <bjorn.gylling@gmail.com>
2010-04-12 12:28:55 +0200

    moved dunder specific code from rdparse.rb to dunder.rb

---
+++ lib/dunder.rb
@@ -0,0 +1,91 @@
+# Dunder language parser
+
+include "rdparse/rdparse.rb"
+
+class Dunder
+  
+  def initialize
+    @dunder_parser = Parser.new("dunder") do
+      token(/\s+/)
+      token(/\d+/) {|m| m.to_i }
+      token(/./) {|m| m }
+      
+      start :statement_list do 
+        match(:statement, :statement_terminator, :statement_list)
+        match(:statement, :statement_terminator)
+      end
+      
+      rule :statement_terminator do
+        match(';')
+        match("\n")
+      end
+      
+      rule :statement do 
+        match(:assignment_expression)
+        match(:function_call)
+        match(:if_statement)
+        match(:while_statement)
+        match(:function_def)
+      end
+
+      rule :assignment_expression do
+        match(:identifier, '=', :expression)
+      end
+      
+      rule :function_call do
+        match(:identifier, :arguments)
+      end
+      
+      rule :arguments do
+        match('(', :expression_list, ')')
+      end
+        
+      rule :expression_list do
+        match(:expression_list, ',', :expression)
+        match(:expression)
+      end
+      
+      rule :expression do
+        match('true')
+        match('false')
+        match(:number)
+        match(:string)
+        match(:indetifier)
+        match(:function_call)
+        match(:expression, :binary_operator, :expression)
+      end
+      
+      rule :number do
+        match(:integer)
+        match(:float)
+      end
+      
+      rule :integer do
+        match(:non_zero_digit, :digits)
+        match('0')
+      end
+      
+      rule :non_zero_digit do
+        match("1-9")
+      end
+      
+      rule :digits do
+        match(:digits, :digit)
+        match(:digit)
+      end
+      
+      rule :digit do
+        match("0-9")
+      end
+      
+    end
+  end
+
+  def log(state = true)
+    if state
+      @dunder_parser.logger.level = Logger::DEBUG
+    else
+      @dunder_parser.logger.level = Logger::WARN
+    end
+  end
+end
