b87b8e2baf78287ff127978576854c037a29b2fa
Björn Gylling <bjorn@ip09-bjogy661.(none)>
2010-04-20 12:16:24 +0200

    math priority now works, statement_list working as well.

--- lib/dunder/nodes.rb
+++ lib/dunder/nodes.rb
@@ -1,45 +1,46 @@
 module Dunder
   module Nodes
-    
+
     class Node
-      
+
       def is_node?
         true
       end
-      
+
     end
-    
+
     class Addition < Node
       attr_accessor :lh, :rh
-      
+
       def eval()
         (@lh.is_node? ? @lh.eval : @lh) + (@rh.is_node? ? @rh.eval : @rh)
       end
     end
-    
+
     class Subtraction < Node
       attr_accessor :lh, :rh
-      
+
       def eval()
-        @lh - @rh
+        (@lh.is_node? ? @lh.eval : @lh) - (@rh.is_node? ? @rh.eval : @rh)
       end
     end
-    
+
     class Multiplication < Node
       attr_accessor :lh, :rh
-      
+
       def eval()
-        @lh * @rh
+        (@lh.is_node? ? @lh.eval : @lh) * (@rh.is_node? ? @rh.eval : @rh)
       end
     end
-    
+
     class Division < Node
       attr_accessor :lh, :rh
-      
+
       def eval()
-        @lh / @rh
+        (@lh.is_node? ? @lh.eval : @lh) / (@rh.is_node? ? @rh.eval : @rh)
       end
     end
-    
+
   end
 end
+
