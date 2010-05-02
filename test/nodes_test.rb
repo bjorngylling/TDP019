af7cef3ea150438f82e2eed2f6dc8043d5036325
Björn Gylling <bjorn.gylling@gmail.com>
2010-05-02 21:21:28 +0200

    Remade nodes_test and helpers to support any kind of int using method missing for easy creation of DInteger nodes.

--- test/nodes_test.rb
+++ test/nodes_test.rb
@@ -2,52 +2,47 @@ require "test/test_helpers.rb"
 
 class DunderNodesTest < Test::Unit::TestCase
 
-  def setup
-    @int_10 = create_node "dinteger", "10"
-    @int_5 = create_node "dinteger", "5"
-  end
-
   def test_addition
-    assert_equal 15, create_node(method_name, @int_10, @int_5).eval
+    assert_equal 15, create_node(method_name, int_10, int_5).eval
   end
 
   def test_subtraction
-    assert_equal 5, create_node(method_name, @int_10, @int_5).eval
+    assert_equal 5, create_node(method_name, int_10, int_5).eval
   end
 
   def test_multiplication
-    assert_equal 50, create_node(method_name, @int_10, @int_5).eval
+    assert_equal 50, create_node(method_name, int_10, int_5).eval
   end
 
   def test_division
-    assert_equal 2, create_node(method_name, @int_10, @int_5).eval
+    assert_equal 2, create_node(method_name, int_10, int_5).eval
   end
 
   def test_advanced_addition
     # Create another Addition Node and use it as the left hand
-    sub_addition = create_node "addition", @int_5, @int_5
+    sub_addition = create_node method_name, int_5, int_6
 
-    assert_equal 20, create_node(method_name, sub_addition, @int_10).eval
+    assert_equal 21, create_node(method_name, sub_addition, int_10).eval
   end
 
   def test_dboolean
     assert_equal true, create_node(method_name, true).eval
     assert_equal false, create_node(method_name, false).eval
     assert_equal false, create_node(method_name, nil).eval
-    assert_equal false, create_node(method_name, create_node("dinteger", "0")).eval
-    assert_equal true, create_node(method_name, create_node("dinteger", "1")).eval
+    assert_equal false, create_node(method_name, int_0).eval
+    assert_equal true, create_node(method_name, int_1).eval
     assert_equal true, create_node(method_name, create_node("dstring", "Yarrr!")).eval
   end
 
   def test_comparison
-    assert_equal true, create_node(method_name, @int_10, "==", @int_10).eval
-    assert_equal false, create_node(method_name, @int_10, "==", @int_5).eval
-    assert_equal true, create_node(method_name, @int_10, "!=", @int_5).eval
-    assert_equal false, create_node(method_name, @int_5, "!=", @int_5).eval
+    assert_equal true, create_node(method_name, int_10, "==", int_10).eval
+    assert_equal false, create_node(method_name, int_10, "==", int_5).eval
+    assert_equal true, create_node(method_name, int_10, "!=", int_5).eval
+    assert_equal false, create_node(method_name, int_5, "!=", int_5).eval
   end
 
   def test_ifstatement
-    stmt_list = create_node("statementlist", create_node("addition", @int_5, @int_5))
+    stmt_list = create_node("statementlist", create_node("addition", int_5, int_5))
     condition = create_node("dboolean", true)
 
     assert_equal 10, create_node(method_name, condition, stmt_list).eval
@@ -57,7 +52,7 @@ class DunderNodesTest < Test::Unit::TestCase
     scope = {} # Empty variable scope to store our test-variable
 
     # Make the assignment
-    create_node(method_name, "myVar", @int_10).eval(scope)
+    create_node(method_name, "myVar", int_10).eval(scope)
 
     assert_equal 10, scope[:myVar]
   end
@@ -67,12 +62,12 @@ class DunderNodesTest < Test::Unit::TestCase
     scope = {"PARENTSCOPE" => global_scope} # Empty variable scope to store our test-variable
 
     # Make the assignment
-    create_node(method_name, "myVar", @int_10).eval(scope)
+    create_node(method_name, "myVar", int_10).eval(scope)
     assert_equal 10, scope[:myVar]
     assert_nil global_scope[:myVar] # Should not pollute parent-scope
 
     # This time it should find "another_variable" in the parent-scope and overwrite it
-    create_node(method_name, "another_variable", @int_5).eval(scope)
+    create_node(method_name, "another_variable", int_5).eval(scope)
     assert_equal 5, global_scope[:another_variable]
     assert_nil scope[:another_variable] # Should not pollute other scopes
   end
@@ -93,9 +88,9 @@ class DunderNodesTest < Test::Unit::TestCase
 
   def test_statementlist
     # Create a bunch of statements
-    stmt_1 = create_node "addition", @int_5, @int_10
-    stmt_2 = create_node "addition", @int_5, @int_5
-    stmt_3 = create_node "multiplication", @int_5, @int_10
+    stmt_1 = create_node "addition", int_5, int_10
+    stmt_2 = create_node "addition", int_5, int_5
+    stmt_3 = create_node "multiplication", int_5, int_10
 
     stmt_list_1 = create_node method_name, stmt_1
 
