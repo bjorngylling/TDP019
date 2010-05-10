aab7099bc97b2d3ed7af6204cea67f8d1cc79190
Björn Gylling <bjorn.gylling@gmail.com>
2010-05-10 02:38:04 +0200

    VERSION 1.0.0

--- lib/dunder.rb
+++ lib/dunder.rb
@@ -1,11 +1,13 @@
 # Require all files in lib/dunder
 $:.unshift File.dirname(__FILE__)
-Dir["#{File.dirname(__FILE__)}/dunder/*.rb"].each { |format| require "dunder/#{File.basename format}" }
+Dir["#{File.dirname(__FILE__)}/dunder/*.rb"].each do 
+  |format| require "dunder/#{File.basename format}"
+end
   
 def evaluate_file(file_name)
   global_scope = Hash.new
-  # code to evaluate passed file
-  puts Dunder::Parser.new.parse(read_file_to_string(file_name)).eval(global_scope)
+  file = read_file_to_string(file_name)
+  puts Dunder::Parser.new.parse(file).eval(global_scope)
 end
 
 def read_file_to_string(file_name)
@@ -14,9 +16,7 @@ end
 
 options = {:v => Proc.new { puts read_file_to_string("VERSION") }, 
            :ip => Proc.new { Dunder::Parser.new.interactive_parser }}
-
-# a flag to start a interactive interpreter
-# a flag to check version
+		   
 if !ARGV.empty?
   flag = ARGV.first.delete("-").to_sym
   if options.has_key? flag
